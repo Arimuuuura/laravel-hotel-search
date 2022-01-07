56450fafb40a0bb07b86d70d10467e48a52e1bbc
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-08 01:22:35 +0900

    update select box style

--- src/resources/views/components/search/select.blade.php
+++ src/resources/views/components/search/select.blade.php
@@ -3,19 +3,52 @@
     $small = \Request::get("small") ?? false;
     $detail = \Request::get("detail") ?? false;
 @endphp
-<select name="middle" id="middle" class="mb-2 lg:mb-0 lg:mr-2">
-    <optgroup label="都道府県を選択" />
-    <option value="">
-        未選択
-    </option>
-    @foreach($areas as $area)
-        <option value="{{ $area["middleClass"][0]["middleClassCode"] }}" @if(\Request::get("middle") === $area["middleClass"][0]["middleClassCode"]) selected @endif>
-            {{ $area["middleClass"][0]["middleClassName"] }}
-        </option>
-    @endforeach
-</select>
-<select name="small" id="small" class="mb-2 lg:mb-0 lg:mr-2"></select>
-<select name="detail" id="detail" class="mb-2 lg:mb-0 lg:mr-2"></select>
+<div class="ml-6 items-center">
+    <span class="mr-3 text-xs text-gray-500">都道府県を選択</span>
+    <div class="relative">
+        <select name="middle" id="middle" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+            <optgroup label="都道府県を選択" />
+            <option value="">
+                未選択
+            </option>
+            @foreach($areas as $area)
+                <option value="{{ $area["middleClass"][0]["middleClassCode"] }}" @if(\Request::get("middle") === $area["middleClass"][0]["middleClassCode"]) selected @endif>
+                    {{ $area["middleClass"][0]["middleClassName"] }}
+                </option>
+            @endforeach
+        </select>
+        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+              <path d="M6 9l6 6 6-6"></path>
+            </svg>
+        </span>
+    </div>
+</div>
+<div class="ml-6 items-center">
+    <span class="mr-3 text-xs text-gray-500">エリアを選択</span>
+    <div class="relative">
+        <select name="small" id="small" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+        </select>
+        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+              <path d="M6 9l6 6 6-6"></path>
+            </svg>
+        </span>
+    </div>
+</div>
+<div class="ml-6 items-center">
+    <span class="mr-3 text-xs text-gray-500">詳細エリア</span>
+    <div class="relative">
+        <select name="detail" id="detail" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+        </select>
+        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+              <path d="M6 9l6 6 6-6"></path>
+            </svg>
+        </span>
+    </div>
+</div>
+
 
 <script>
     'use strict';
@@ -25,6 +58,7 @@
         const detail = document.getElementById('detail');
         const smallEx = "<?= $small ?>";
         const detailEx = "<?= $detail ?>";
+        const areas = <?= $areasJson ?>;
 
         const createOption = (area, code, name, className, city) => {
             const option = document.createElement('option');
@@ -50,40 +84,32 @@
             }
         };
 
-        const createSmallSelects = value => {
-            const middleArea = value;
-            const areas = <?= $areasJson ?>;
+        const createSmallSelects = middleArea => {
             removeOption('small-option', small);
 
-            areas.forEach(element => {
-                const middleCode = element["middleClass"][0]["middleClassCode"];
-                const smalls = element["middleClass"][1]["smallClasses"];
-                if(middleArea === middleCode){
-                    smalls.forEach(element => {
-                        const smallCode = element["smallClass"][0]["smallClassCode"];
-                        const smallName = element["smallClass"][0]["smallClassName"];
-                        createOption(small, smallCode, smallName, 'small-option', smallEx);
-                    });
-                }
+            areas.forEach(middleClasses => {
+                const middleCode = middleClasses["middleClass"][0]["middleClassCode"];
+                const smalls = middleClasses["middleClass"][1]["smallClasses"];
+                middleArea === middleCode && smalls.forEach(smallClasses => {
+                    const smallCode = smallClasses["smallClass"][0]["smallClassCode"];
+                    const smallName = smallClasses["smallClass"][0]["smallClassName"];
+                    createOption(small, smallCode, smallName, 'small-option', smallEx);
+                });
             });
         }
 
-        const createDetailSelects = value => {
-            const smallArea = value;
-            const areas = <?= $areasJson ?>;
+        const createDetailSelects = smallArea => {
             removeOption('detail-option', detail);
 
-            areas.forEach(element => {
-                if(element["middleClass"][1]["smallClasses"][0]["smallClass"].length > 1) {
-                    const smallCode = element["middleClass"][1]["smallClasses"][0]["smallClass"][0]["smallClassCode"];
-                    if(smallArea === smallCode){
-                        const details = element["middleClass"][1]["smallClasses"][0]["smallClass"][1]["detailClasses"];
-                        details.forEach(e => {
-                            const detailCode = e["detailClass"]["detailClassCode"];
-                            const detailName = e["detailClass"]["detailClassName"];
-                            createOption(detail, detailCode, detailName, 'detail-option', detailEx);
-                        });
-                    }
+            areas.forEach(middleClasses => {
+                if(middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"].length > 1) {
+                    const smallCode = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][0]["smallClassCode"];
+                    const details = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][1]["detailClasses"];
+                    smallArea === smallCode && details.forEach(detailClasses => {
+                        const detailCode = detailClasses["detailClass"]["detailClassCode"];
+                        const detailName = detailClasses["detailClass"]["detailClassName"];
+                        createOption(detail, detailCode, detailName, 'detail-option', detailEx);
+                    });
                 }
             });
         }
@@ -99,6 +125,7 @@
         // selectbox middle が変更された際のイベント
         middle.addEventListener('change', e => {
             createSmallSelects(e.target.value);
+            removeOption('detail-option', detail);
         });
 
         // selectbox small が変更された際のイベント
