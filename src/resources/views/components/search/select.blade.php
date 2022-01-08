8ed6d22f014b508ee2b76db3706555ff9d29511f
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-09 00:26:48 +0900

    検索ボタンの活性、非活性

--- src/resources/views/components/search/select.blade.php
+++ src/resources/views/components/search/select.blade.php
@@ -3,52 +3,64 @@
     $small = \Request::get("small") ?? false;
     $detail = \Request::get("detail") ?? false;
 @endphp
-<div class="ml-6 items-center">
-    <span class="mr-3 text-xs text-gray-500">都道府県を選択</span>
-    <div class="relative">
-        <select name="middle" id="middle" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-            <optgroup label="都道府県を選択" />
-            <option value="">
-                未選択
-            </option>
-            @foreach($areas as $area)
-                <option value="{{ $area["middleClass"][0]["middleClassCode"] }}" @if(\Request::get("middle") === $area["middleClass"][0]["middleClassCode"]) selected @endif>
-                    {{ $area["middleClass"][0]["middleClassName"] }}
-                </option>
-            @endforeach
-        </select>
-        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-              <path d="M6 9l6 6 6-6"></path>
-            </svg>
-        </span>
+<form method="get" action="{{ route('hotel.search') }}">
+    <div class="lg:flex lg:justify-around">
+        <div class="lg:flex items-center">
+            <div class="flex space-x-2 items-center">
+                <div class="ml-6 items-center">
+                    <span class="mr-3 text-xs text-gray-500">都道府県を選択</span>
+                    <div class="relative">
+                        <!--suppress HtmlFormInputWithoutLabel -->
+                        <select name="middle" id="middle" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                            <!--suppress CheckEmptyScriptTag -->
+                            <optgroup label="都道府県を選択" />
+                            <option value="">
+                                未選択
+                            </option>
+                            @foreach($areas as $area)
+                                <option value="{{ $area["middleClass"][0]["middleClassCode"] }}" @if(\Request::get("middle") === $area["middleClass"][0]["middleClassCode"]) selected @endif>
+                                    {{ $area["middleClass"][0]["middleClassName"] }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+                                <path d="M6 9l6 6 6-6"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+                <div class="ml-6 items-center">
+                    <span class="mr-3 text-xs text-gray-500">エリアを選択</span>
+                    <div class="relative">
+                        <!--suppress HtmlFormInputWithoutLabel -->
+                        <select name="small" id="small" disabled class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                        </select>
+                        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+                                <path d="M6 9l6 6 6-6"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+                <div class="ml-6 items-center">
+                    <span class="mr-3 text-xs text-gray-500">詳細エリア</span>
+                    <div class="relative">
+                        <!--suppress HtmlFormInputWithoutLabel -->
+                        <select name="detail" id="detail" disabled class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                        </select>
+                        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+                                <path d="M6 9l6 6 6-6"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+                <div><button id="hotel-search" type="submit" disabled class="ml-auto text-white bg-gray-300 border-0 py-2 px-6 focus:outline-none rounded">検索する</button></div>
+            </div>
+        </div>
     </div>
-</div>
-<div class="ml-6 items-center">
-    <span class="mr-3 text-xs text-gray-500">エリアを選択</span>
-    <div class="relative">
-        <select name="small" id="small" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-        </select>
-        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-              <path d="M6 9l6 6 6-6"></path>
-            </svg>
-        </span>
-    </div>
-</div>
-<div class="ml-6 items-center">
-    <span class="mr-3 text-xs text-gray-500">詳細エリア</span>
-    <div class="relative">
-        <select name="detail" id="detail" class="text-gray-700 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-        </select>
-        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-              <path d="M6 9l6 6 6-6"></path>
-            </svg>
-        </span>
-    </div>
-</div>
-
+</form>
 
 <script>
     'use strict';
@@ -56,6 +68,7 @@
         const middle = document.getElementById('middle');
         const small = document.getElementById('small');
         const detail = document.getElementById('detail');
+        const searchButton = document.getElementById('hotel-search');
         const smallEx = "<?= $small ?>";
         const detailEx = "<?= $detail ?>";
         const areas = <?= $areasJson ?>;
@@ -84,13 +97,25 @@
             }
         };
 
+        const classes = areas.map(val => {
+            const {middleClass} = val;
+            const [{middleClassCode, middleClassName}] = middleClass;
+            // const smallClasses = middleClass.map((val, index) => {
+            //     if(!index === 1) return;
+            //     const {smallClasses} = val;
+            //     return {smallClasses};
+            // });
+            return {middleClassCode, middleClassName};
+        });
+
         const createSmallSelects = middleArea => {
             removeOption('small-option', small);
+            small.disabled = false;
 
-            areas.forEach(middleClasses => {
-                const middleCode = middleClasses["middleClass"][0]["middleClassCode"];
+            areas.forEach((middleClasses, index) => {
+                const {middleClassCode, middleClassName} = classes[index];
                 const smalls = middleClasses["middleClass"][1]["smallClasses"];
-                middleArea === middleCode && smalls.forEach(smallClasses => {
+                middleArea === middleClassCode && smalls.forEach(smallClasses => {
                     const smallCode = smallClasses["smallClass"][0]["smallClassCode"];
                     const smallName = smallClasses["smallClass"][0]["smallClassName"];
                     createOption(small, smallCode, smallName, 'small-option', smallEx);
@@ -102,16 +127,25 @@
             removeOption('detail-option', detail);
 
             areas.forEach(middleClasses => {
-                if(middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"].length > 1) {
-                    const smallCode = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][0]["smallClassCode"];
-                    const details = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][1]["detailClasses"];
-                    smallArea === smallCode && details.forEach(detailClasses => {
-                        const detailCode = detailClasses["detailClass"]["detailClassCode"];
-                        const detailName = detailClasses["detailClass"]["detailClassName"];
-                        createOption(detail, detailCode, detailName, 'detail-option', detailEx);
-                    });
-                }
+                if(middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"].length === 1) return;
+                const smallCode = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][0]["smallClassCode"];
+                const details = middleClasses["middleClass"][1]["smallClasses"][0]["smallClass"][1]["detailClasses"];
+                smallArea === smallCode && details.forEach(detailClasses => {
+                    const detailCode = detailClasses["detailClass"]["detailClassCode"];
+                    const detailName = detailClasses["detailClass"]["detailClassName"];
+                    createOption(detail, detailCode, detailName, 'detail-option', detailEx);
+                });
             });
+        }
+
+        const isActivationButton = flag => {
+            if (flag) {
+                searchButton.disabled = flag;
+                searchButton.className = 'ml-auto text-white bg-gray-300 border-0 py-2 px-6 focus:outline-none rounded';
+            } else {
+                searchButton.disabled = flag;
+                searchButton.className = 'ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded';
+            }
         }
 
         createOptgroup();
@@ -126,11 +160,19 @@
         middle.addEventListener('change', e => {
             createSmallSelects(e.target.value);
             removeOption('detail-option', detail);
+            small.value === '0' && isActivationButton(true);
         });
 
         // selectbox small が変更された際のイベント
         small.addEventListener('change', e => {
             createDetailSelects(e.target.value);
+            detail.disabled = detail.length <= 1;
+            detail.length === 1 ? isActivationButton(false) : isActivationButton(true);
+        });
+
+        // selectbox detail が変更された際のイベント
+        detail.addEventListener('change', () => {
+            isActivationButton(false);
         });
     }
 </script>
--- src/resources/views/search/search.blade.php
+++ src/resources/views/search/search.blade.php
@@ -9,16 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="get" action="{{ route('hotel.search') }}">
-                        <div class="lg:flex lg:justify-around">
-                            <div class="lg:flex items-center">
-                                <div class="flex space-x-2 items-center">
+{{--                    <form method="get" action="{{ route('hotel.search') }}">--}}
+{{--                        <div class="lg:flex lg:justify-around">--}}
+{{--                            <div class="lg:flex items-center">--}}
+{{--                                <div class="flex space-x-2 items-center">--}}
                                     <x-search.select :areas="$areas" />
-                                    <div><button type="submit" class="ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">検索する</button></div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+{{--                                    <div><button id="hotel-search" type="submit" class="ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">検索する</button></div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
                     @if($hotels !== null)
                         <x-search.container>
                             @foreach($hotels as $hotel)
